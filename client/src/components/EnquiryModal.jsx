805fb1fda354da2795f85ae848a27ecba0148de2
BM-ACADEMY <teamkamarweb@gmail.com>
2025-07-15 12:47:02 +0530

    frontend update

--- client/src/components/EnquiryModal.jsx
+++ client/src/components/EnquiryModal.jsx
@@ -16,7 +16,7 @@ export default function EnquiryModal({ isOpen, onClose }) {
   setIsLoading(true);
 
   try {
-    const res = await fetch("http://localhost:5000/send-email", {
+    const res = await fetch("https://abmgroupswebsite.onrender.com/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
